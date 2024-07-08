385f536cdfd0624881ab634c25ed51a96589576a
Jai <jayasankar2303@gmail.com>
2024-07-08 20:53:15 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package demo;
+
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		System.out.println("Rama");
+		String name = "jai";
+		
+		for(int i=0;i<name.length();i++) {
+			System.out.println(name.charAt(i));
+		}
+		
+	}
+
+}
